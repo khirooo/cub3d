a036f7ae310603b678f2ee3568a2d832e591335b
Kheireddine Fergani <kfergani@1-D-19.42heilbronn.de>
2022-11-09 14:19:14 +0100

    updated

--- src/mlx.c
+++ src/mlx.c
@@ -6,7 +6,7 @@
 /*   By: kfergani <kfergani@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/05 21:40:13 by kfergani          #+#    #+#             */
-/*   Updated: 2022/11/06 21:26:00 by kfergani         ###   ########.fr       */
+/*   Updated: 2022/11/09 13:56:34 by kfergani         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,18 +20,52 @@ void	my_mlx_pixel_put(t_image *image, int x, int y, int color)
 	*(unsigned int*)dst = color;
 }
 
+int	window_init(t_window **wind)
+{
+	*wind = (t_window *)malloc(sizeof(t_window));
+	if (!*wind)
+		return (1);
+	(*wind)->mlx = mlx_init();
+	(*wind)->win = mlx_new_window((*wind)->mlx, 1024, 512, "Cub3D");
+	(*wind)->image = (t_image *)malloc(sizeof(t_image));
+	if (!*wind)
+		return (1);
+	(*wind)->image->img = mlx_new_image((*wind)->mlx, 1024, 512);
+	(*wind)->image->addr = mlx_get_data_addr((*wind)->image->img, &((*wind)->image->bits_per_pixel),
+						&((*wind)->image->line_length), &((*wind)->image->endian));
+	(*wind)->pos.x = 22;
+	(*wind)->pos.y = 12;
+	(*wind)->dir.x = -1;
+	(*wind)->dir.x = 0;
+	(*wind)->plan.x = 0;
+	(*wind)->plan.y = 0.66;
+	return (0);
+}
+
+void	raycast(t_window *wind, t_scene *scene)
+{
+	int	x;
+	t_point	cam;
+	t_point	raydir;
+	t_point	map;
+
+	x = 0;
+	while (x < 512)
+	{
+		cam.x = ((2 * x) / 512) - 1;
+		
+		x++;
+	}
+	
+}
+
 void	open_window(t_scene *scene)
 {
 	t_window *wind;
 
 	wind = (t_window *)malloc(sizeof(t_window));
-	if (!wind || !scene)
+	if (window_init(&wind) || !scene)
 		return ;
-	wind->mlx = mlx_init();
-	wind->win = mlx_new_window(wind->mlx, 1024, 512, "Cub3D");
-	wind->image = (t_image *)malloc(sizeof(t_image));
-	wind->image->img = mlx_new_image(wind->mlx, 1024, 512);
-	wind->image->addr = mlx_get_data_addr(wind->image->img, &(wind->image->bits_per_pixel), &(wind->image->line_length), &(wind->image->endian));
 	my_mlx_pixel_put(wind->image, 512, 256, 0x00FF0000);
 	mlx_put_image_to_window(wind->mlx, wind->win, wind->image->img, 0, 0);
 	mlx_loop(wind->mlx);
